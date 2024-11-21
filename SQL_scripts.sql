56097f7765467cdc246245d0c1ca2b05a05a4634
Grigoriy <g.dombayan@modulbank.ru>
2024-11-21 10:17:49 +0300

    SQL-скрипты

---
+++ SQL_scripts.sql
@@ -0,0 +1,10 @@
+-- Выборка самых молодых жителей по странам в виде представления
+
+create view the_oldest_people_by_countries (fio, age,dob,country_name) as
+
+with rn_cte as (select name, age, dob, country_id, row_number() over (partition by country_id order by dob) rn
+				from people)
+
+select rn.name, rn.age, rn.dob, ct.name
+from rn_cte rn join countries ct on rn.country_id = ct.id 
+where rn.rn = 1;
